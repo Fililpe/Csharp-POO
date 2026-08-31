623241ace5f9fee7a1f0c44d181cf8bc156b67d6
Filipe <lfilipesousa17@gmail.com>
2026-08-30 22:37:11 -0300

    feat: exercicio 01 - IMC

---
+++ Exercicio-01/Program.cs
@@ -0,0 +1,25 @@
+﻿
+Console.WriteLine("=== AVISO === \n *NÃO use . como separador*");
+
+Console.WriteLine("Peso: ");
+
+string? entrada1 = Console.ReadLine();
+if (entrada1 == null)
+{
+    Console.WriteLine("Entrada inválida, encerrando...");
+    return;
+}
+double peso = double.Parse(entrada1);
+
+Console.WriteLine("Altura: ");
+string? entrada2 = Console.ReadLine();
+if (entrada2 == null)
+{
+    Console.WriteLine("Entrada inválida, encerrando...");
+    return;
+}
+double altura = double.Parse(entrada2);
+
+double imc = peso / (altura * altura);
+
+Console.WriteLine($"o imc do peso: {peso}kg e altura:{altura}m é IMC={imc:f2}");
