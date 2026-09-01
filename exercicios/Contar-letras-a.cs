6bc203ec2d822117ab1266ec00a5f1d90f6b6a22
Filipe <lfilipesousa17@gmail.com>
2026-08-31 23:29:19 -0300

    Adiciona gitignore e remove artefatos de build

---
+++ exercicios/Contar-letras-a.cs
@@ -0,0 +1,9 @@
+﻿Console.Write("Informe uma string: ");
+string texto = Console.ReadLine() ?? "";
+
+int cont = 0;
+for (int i = 0; i < texto.Length; i++)
+    if (texto[i] == 'a')
+        cont++;
+
+Console.WriteLine($"A string possui {cont} letras 'a'");
